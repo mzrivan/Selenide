4bbee94e302086c31315c5de6baa0265de728c85
mzrivan <mzr993@gmail.com>
2022-08-16 17:11:38 +0300

    Added static variables

--- src/test/java/ru/netology/web/DeliveryTest.java
+++ src/test/java/ru/netology/web/DeliveryTest.java
@@ -12,46 +12,42 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
-    Date serviceDate = new Date();
-
-    @Test
-    void shouldDeliveryPositiveTest() {
+    static Date serviceDate = new Date();
+    static int afterDay = 7;//Не более 30дней, переключение месяца или года происходит только на один шаг
+    static String planningDate = serviceDate.dateAfterDays(afterDay);
+    @BeforeEach
+    void startTest(){
         open("http://localhost:9999");
-        String planningDate = serviceDate.dateAfterDays(5);
-        $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(planningDate);
-        $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("+79270000000");
+    }
+    @AfterEach
+    void completionTest(){
         $("[data-test-id=agreement] span").click();
         $("[role=button].button").click();
         $(".notification__content")
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
     }
+    @Test
+    void shouldDeliveryPositiveTest() {
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+    }
 
     @Test
     void shouldDeliveryCityChoicePositiveTest() {
-        open("http://localhost:9999");
-        String planningDate = serviceDate.dateAfterDays(7);
         $("[data-test-id=city] input").setValue("Бе");
         $(byText("Белгород")).click();
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=agreement] span").click();
-        $("[role=button].button").click();
-        $(".notification__content")
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
-                .shouldBe(Condition.visible);
     }
 
     @Test
     void shouldDeliveryCalendarChoicePositiveTest() {
-        open("http://localhost:9999");
-        int afterDay = 7;//Не более 30дней, переключение месяца или года происходит только на один шаг
-        String planningDate = serviceDate.dateAfterDays(afterDay);
         $("[data-test-id=city] input").setValue("Воронеж");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         if (serviceDate.isNextYear(afterDay)) {
@@ -63,11 +59,6 @@ class DeliveryTest {
         $$(".calendar__day").find(exactText(serviceDate.dayAfterDays(afterDay))).click();
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=agreement] span").click();
-        $("[role=button].button").click();
-        $(".notification__content")
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
-                .shouldBe(Condition.visible);
     }
 
 }
